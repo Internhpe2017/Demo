730c25c870bb716dd8be488d4f3f46bc7d5d6276
dhayanithi007 <dhayanithivasan@gmail.com>
2017-04-26 18:09:38 +0530

    jfrog

--- jfroginstall/manifests/install.pp
+++ jfroginstall/manifests/install.pp
@@ -11,7 +11,7 @@ class jfroginstall::install
            ensure => 'directory',
            group   => 'jfrog',
            owner  => 'jfrog',
-            mode  => '655',
+            mode  => '755',
           }
 
  file{"/opt/software/jfrog/jfrog-artifactory-oss-5.1.3.zip":
@@ -23,12 +23,12 @@ class jfroginstall::install
         mode => 755,
      }->
 
-exec{"unzip":
-	cwd=>'/opt/software/jfrog/',
-	path=>["/usr/bin", "/usr/sbin", "/bin","/sbin","/usr"],
-     command=>"unzip jfrog-artifactory-oss-5.1.3.zip",
+#exec{"unzip":
+	#cwd=>'/opt/software/jfrog/',
+	#path=>["/usr/bin", "/usr/sbin", "/bin","/sbin","/usr"],
+    # command=>"unzip jfrog-artifactory-oss-5.1.3.zip",
             
-     }->
+    # }->
 
  exec { 'remove':
 	path=>["/usr/bin", "/usr/sbin", "/bin","/sbin","/usr"],
@@ -45,10 +45,15 @@ exec{"unzip":
  exec { 'permission':
 	#require => File["/opt/jfrog/artifactory-oss-4.6.0/bin/artifactory.default"],
 	path    => ["/sbin","/usr","/usr/bin", "/usr/sbin", "/bin"],
-	cwd => "/opt/software/jfrog/artifactory-oss-5.1.3/bin",
-	command => "chmod 755 * ",
-    #command => " chmod a+x ",
-    }->
+	cwd => "/opt",
+	command => "chmod -R 755 *",
+ }->
+exec { 'ownership':
+  require => Exec["permission"],
+  path    => ["/usr","/bin","/sbin","/usr/bin", "/usr/sbin"],
+  command => "chown -R jfrog:jfrog /opt/software/jfrog",
+  }->
+
  
 exec{"installservice":
 	path=>["/usr/bin", "/usr/sbin", "/bin","/sbin","/usr"],
@@ -68,12 +73,3 @@ exec{"switchuser":
           command => "service artifactory start",
       }   
    }
-
-
-
-/*exec { 'ownership':
-require => Exec["permission"],
-path    => ["/usr","/bin","/sbin","/usr/bin", "/usr/sbin"],
-command => "chown -R jfrog:jfrog /opt/software/jfrog",
-}*/
-
