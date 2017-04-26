1d43b5207729355dfe7140c7a76a1e919589b7ea
narendrahm <narendrahmn@gmail.com>
2017-04-26 10:35:56 +0530

    jeera
    rakesh

--- init.pp
+++ init.pp
@@ -1,25 +1,31 @@
-class  git_install {
-        $var="git_dep.tar.gz"
-        Exec {
-                path => ['/bin','/sbin/','/usr/bin/','/usr/sbin/'],
+class jira 
+{
+	require 'java'
+	require 'Mysql'
+        $bin = "atlassian-jira-software-7.2.3-x64.bin"
+        file { '/opt/software/jira/':
+                        ensure => 'directory',
+                        mode   => '0750',
+                }
+
+        file{"$bin":
+                        path =>"/opt/software/jira/$bin",
+                        source=>"puppet:///modules/jira/files/{$bin}",
+                        ensure=>present,
+			mode=>755,
+        }
+       /* exec{"chmod 755 $bin":
+                         cwd=>'/opt/software/jira',
+                       command=>'chmod 755 atlassian-jira-software-7.2.3-x64.bin',
+                        path=>['/usr/bin','/usr/sbin',],
+        }*/
+         exec{"./$bin":
+                        #cwd=>'/opt/software/jira/',
+                        command=>'/opt/software/jira/atlassian-jira-software-7.2.3-x64.bin',
+                        path=>['/bin/','/sbin/','/usr/bin/','/usr/sbin/','/opt/software/jira/',],
         }
 
-        file { "/opt/software/${var}":
-                 path => "/opt/software/${var}",
-                 source => "puppet:///modules/git_install/${var}",
-                 ensure => "present",
-                 owner => root,
-                 mode => 777,
-                 }
-        exec { "git1":
-                cwd => "/opt/software/",
-                 path => ['/bin','/sbin/','/usr/bin/','/usr/sbin/'],
-                command => "tar -xzvf ${var}",
-                require => File["/opt/software/${var}"],
-             }
-        exec { "git2":
-                cwd => "/opt/software/",
-                command => "yum localinstall *.rpm",
-                require => Exec["git1"],
-             }
 }
+
+
+
